c312db2247f61a8feaf7442b509e899757c6a987
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-02-09 18:48:25 -0300

    Merge manual feita para os requisitos 3,4 e 5.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ProductCard from '../components/ProductCard';
 import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class Home extends Component {
@@ -7,6 +9,16 @@ export default class Home extends Component {
     noSearched: false,
     category: '',
     productList: [],
+    categories: [],
+  };
+
+  componentDidMount() {
+    this.setCategories();
+  }
+
+  setCategories = async () => {
+    const data = await getCategories();
+    this.setState({ categories: data });
   };
 
   handleChange = ({ target }) => {
@@ -26,32 +38,44 @@ export default class Home extends Component {
     });
   };
 
-  constructor() {
-    super();
-
-    this.state = {
-      categories: [],
-    };
-  }
-
-  componentDidMount() {
-    this.setCategories();
-  }
-
-  setCategories = async () => {
-    const data = await getCategories();
-    this.setState({ categories: data });
-  };
-
   render() {
+    const { searchText, productList, noSearched, categories } = this.state;
+
     return (
       <div>
-        <p
-          data-testid="home-initial-message"
-          id="inicialMessage"
-        >
-          Digite algum termo de pesquisa ou escolha uma categoria.
+        <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
+        <p>
+          {categories.map((item) => (
+            <label key={ item.id } data-testid="category" htmlFor={ item.id }>
+              <input type="radio" name="category" id={ item.id } />
+            </label>
+          ))}
         </p>
+        <input
+          type="text"
+          name="searchText"
+          value={ searchText }
+          onChange={ this.handleChange }
+          data-testid="query-input"
+        />
+        <button
+          data-testid="query-button"
+          name="searchButton"
+          onClick={ this.handleClick }
+        >
+          Pesquisar
+        </button>
+        { (productList.length === 0 && noSearched)
+        && <h2>Nenhum produto foi encontrado</h2> }
+        { (productList.length === 0 && !noSearched) && (
+          <p
+            data-testid="home-initial-message"
+            id="inicialMessage"
+          >
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        )}
+        <ProductCard productList={ productList } />
       </div>
     );
   }
