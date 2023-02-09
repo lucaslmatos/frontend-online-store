781d22025d10a2fa7e76ad8419064b64c37ec3ef
Lucas Matos <lucaslmatos18@gmail.com>
2023-02-09 18:24:11 -0300

    REQ5 - Finalizado, criado componente ProductCard, e interação com a página Home

---
+++ src/components/ProductCard.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class ProductCard extends Component {
+  render() {
+    const { productList } = this.props;
+    return (
+      <div>
+        {productList.map(({ title, thumbnail, price }) => (
+          <div key={ title } data-testid="product">
+            <p>{ title }</p>
+            <img src={ thumbnail } alt={ title } />
+            <p>{ price }</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+ProductCard.propTypes = {
+  productList: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    thumbnail: PropTypes.string,
+    price: PropTypes.number,
+  })).isRequired,
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,62 @@
 import React, { Component } from 'react';
+import ProductCard from '../components/ProductCard';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class Home extends Component {
+  state = {
+    searchText: '',
+    noSearched: false,
+    category: '',
+    productList: [],
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleClick = async () => {
+    const { searchText, category } = this.state;
+    const response = await getProductsFromCategoryAndQuery(category, searchText);
+    const data = response.results;
+    this.setState({
+      productList: data,
+      noSearched: true,
+    });
+  };
+
   render() {
+    const { searchText, productList, noSearched } = this.state;
+
     return (
       <div>
-        <p
-          data-testid="home-initial-message"
-          id="inicialMessage"
+        <input
+          type="text"
+          name="searchText"
+          value={ searchText }
+          onChange={ this.handleChange }
+          data-testid="query-input"
+        />
+        <button
+          data-testid="query-button"
+          name="searchButton"
+          onClick={ this.handleClick }
         >
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+          Pesquisar
+        </button>
+        { (productList.length === 0 && noSearched)
+        && <h2>Nenhum produto foi encontrado</h2> }
+        { (productList.length === 0 && !noSearched) && (
+          <p
+            data-testid="home-initial-message"
+            id="inicialMessage"
+          >
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        )}
+        <ProductCard productList={ productList } />
       </div>
     );
   }
