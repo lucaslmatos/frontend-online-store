93d7de5c9326998ecad73dbf96f634978a4b77fe
Fernanda Carvalho de Macêdo <nandaamacedoo@gmail.com>
2023-02-09 17:13:53 -0300

    Req3 - Finalizado, botão de carrinho vazio implementado na Home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 export default class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route path="/" component={ Home } />
-      </Switch>
+      <>
+        <Switch>
+          <Route path="/cart" component={ Cart } />
+          <Route path="/" component={ Home } />
+        </Switch>
+      </>
     );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
+        <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
         <p
           data-testid="home-initial-message"
           id="inicialMessage"
