5355ae539d6719fc5769123074a2f536f9a84327
Lucas Matos <lucaslmatos18@gmail.com>
2023-02-09 16:28:05 -0300

    REQ2 - Finalizado, criada página Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <Switch>
+        <Route path="/" component={ Home } />
+      </Switch>
     );
   }
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
