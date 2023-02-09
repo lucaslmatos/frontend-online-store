2fbdf94dbd35fff9fb8927e042de3fb9e4dcec91
MarinaPadilha <marina.padilha27@gmail.com>
2023-02-09 17:47:49 -0300

    REQ3 - Finalizado, Listagem de categorias implementada

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
+// import Cart from '';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setCategories();
+  }
+
+  setCategories = async () => {
+    const data = await getCategories();
+    this.setState({
+      categories: data,
+    });
+  };
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <p
@@ -9,6 +30,17 @@ export default class Home extends Component {
           id="inicialMessage"
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <p>
+          {categories.map((item) => (
+            <label key={ item.id } htmlFor={ item.id } data-testid="category">
+              <input
+                type="radio"
+                name="category"
+                id={ item.id }
+              />
+            </label>
+          ))}
         </p>
       </div>
     );
