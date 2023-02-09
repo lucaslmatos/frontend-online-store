113af21142970e55f61435926957a216d704c01f
Fernanda Carvalho de Macêdo <nandaamacedoo@gmail.com>
2023-02-09 17:42:34 -0300

    Req3 - Correção no Lint

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 
 export default class App extends Component {
   render() {
     return (
-      <>
-        <Switch>
-          <Route path="/cart" component={ Cart } />
-          <Route path="/" component={ Home } />
-        </Switch>
-      </>
+      <Switch>
+        <Route path="/cart" component={ Cart } />
+        <Route path="/" component={ Home } />
+      </Switch>
     );
   }
 }
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Cart extends Component {
   render() {
     return (
-        <>
+      <>
         <div>Cart</div>
         <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
-        </>
-    )
+      </>
+    );
   }
 }
