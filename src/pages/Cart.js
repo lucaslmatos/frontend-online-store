87952f38cabeeecbc0dad5f2affd4895c3b04387
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-02-10 11:01:18 -0300

    Adicionado funcionalides que serão usadas mais pra frente

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,48 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 export default class Cart extends Component {
+  state = {
+    productsCart: [],
+  };
+
+  componentDidMount() {
+    this.refreshState();
+  }
+
+  refreshState = () => {
+    const { location } = this.props;
+    const { state } = location;
+    const { productsCart } = state;
+    this.setState({
+      productsCart,
+    });
+  };
+
   render() {
+    const { productsCart } = this.state;
+    const messageElement = (
+      <h2
+        data-testid="shopping-cart-empty-message"
+      >
+        Seu carrinho está vazio
+      </h2>
+    );
     return (
       <>
         <div>Cart</div>
-        <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
+        { productsCart.length === 0 ? messageElement
+          : 'render'}
+
       </>
     );
   }
 }
+
+Cart.propTypes = {
+  location: PropTypes.shape({
+    state: PropTypes.shape({
+      productsCart: PropTypes.arrayOf(undefined),
+    }),
+  }).isRequired,
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
+  state = {
+    productsCart: [],
+  };
+
   render() {
+    const { productsCart } = this.state;
     return (
       <div>
-        <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
+        <Link
+          to={ {
+            pathname: '/cart',
+            state: { productsCart },
+          } }
+          data-testid="shopping-cart-button"
+        >
+          Carrinho
+        </Link>
         <p
           data-testid="home-initial-message"
           id="inicialMessage"
